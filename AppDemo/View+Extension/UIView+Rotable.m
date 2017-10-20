d46a629a0bad33e57f6447d681b335643527c5ba
dvvu <dvvu.uit@gmail.com>
2017-10-20 12:50:47 +0700

    [master] adjust view

--- AppDemo/View+Extension/UIView+Rotable.m
+++ AppDemo/View+Extension/UIView+Rotable.m
@@ -23,7 +23,7 @@
 - (void)enableRotable {
     
     self.rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotationWithGestureRecognizer:)];
-    [[self superview] addGestureRecognizer:self.rotationGesture];
+    [self addGestureRecognizer:self.rotationGesture];
 }
 
 #pragma mark - setRotationGesture
